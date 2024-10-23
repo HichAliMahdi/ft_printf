5ff1f41e00d6086dbcccdef89ae1a8a553b763a0
hali-mah <hichalimahdi@gmail.com>
2024-10-23 14:22:57 +0200

    Added dynamic memory allocation to %x and %X

--- ft_lhex.c
+++ ft_lhex.c
@@ -6,19 +6,80 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:05:06 by hali-mah          #+#    #+#             */
-/*   Updated: 2024/10/22 18:44:26 by hali-mah         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:18:06 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_lhex(unsigned int n, int fd) // handles lowercase hexa //
+static int	get_hex_length(unsigned int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+	{
+		return (1);
+	}
+	while (n > 0)
+	{
+		n /= 16;
+		len++;
+	}
+	return (len);
+}
+
+static void	fill_hex_result(unsigned int n, char *result, int len, char *hex)
+{
+	while (n > 0)
+	{
+		result[--len] = hex[n % 16];
+		n /= 16;
+	}
+}
+
+static void	write_hex_result(char *result, unsigned int n, int fd)
+{
+	int	i;
+
+	if (n == 0)
+	{
+		ft_putchar_fd('0', fd);
+	}
+	else
+	{
+		i = 0;
+		while (result[i] != '\0')
+		{
+			ft_putchar_fd(result[i], fd);
+			i++;
+		}
+	}
+}
+
+int	ft_lhex(unsigned int n, int fd)
 {
 	char	*hex;
+	int		len;
+	char	*result;
 
 	hex = "0123456789abcdef";
-	if (n >= 16)
-		ft_lhex(n / 16, fd);
-	ft_putchar_fd(hex[n % 16], fd);
+	len = get_hex_length(n);
+	result = malloc((len + 1) * sizeof(char));
+	if (!result)
+	{
+		return (-1);
+	}
+	result[len] = '\0';
+	if (n == 0)
+	{
+		result[0] = '0';
+	}
+	else
+	{
+		fill_hex_result(n, result, len, hex);
+	}
+	write_hex_result(result, n, fd);
+	free(result);
 	return (0);
 }
--- ft_uhex.c
+++ ft_uhex.c
@@ -6,19 +6,80 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:23:29 by hali-mah          #+#    #+#             */
-/*   Updated: 2024/10/22 18:45:07 by hali-mah         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:20:03 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_uhex(unsigned int n, int fd) // handles uppercase hexa
+static int	get_hex_length(unsigned int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+	{
+		return (1);
+	}
+	while (n > 0)
+	{
+		n /= 16;
+		len++;
+	}
+	return (len);
+}
+
+static void	fill_hex_result(unsigned int n, char *result, int len, char *hex)
+{
+	while (n > 0)
+	{
+		result[--len] = hex[n % 16];
+		n /= 16;
+	}
+}
+
+static void	write_hex_result(char *result, unsigned int n, int fd)
+{
+	int	i;
+
+	if (n == 0)
+	{
+		ft_putchar_fd('0', fd);
+	}
+	else
+	{
+		i = 0;
+		while (result[i] != '\0')
+		{
+			ft_putchar_fd(result[i], fd);
+			i++;
+		}
+	}
+}
+
+int	ft_uhex(unsigned int n, int fd)
 {
 	char	*hex;
+	int		len;
+	char	*result;
 
 	hex = "0123456789ABCDEF";
-	if (n >= 16)
-		ft_uhex(n / 16, fd);
-	ft_putchar_fd(hex[n % 16], fd);
+	len = get_hex_length(n);
+	result = malloc((len + 1) * sizeof(char));
+	if (!result)
+	{
+		return (-1);
+	}
+	result[len] = '\0';
+	if (n == 0)
+	{
+		result[0] = '0';
+	}
+	else
+	{
+		fill_hex_result(n, result, len, hex);
+	}
+	write_hex_result(result, n, fd);
+	free(result);
 	return (0);
 }
